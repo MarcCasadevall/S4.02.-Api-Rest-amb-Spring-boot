b1c195f0bb16048d341facc54dedec50c367f668
MarcCasadevall <apple5.92@hotmail.com>
2026-05-06 10:59:33 +0200

    feat: add Order and OrderItem model classes for MongoDB

---
+++ fruit-api-h2/src/main/java/cat/itacademy/s04/t02/n01/fruit/model/Order.java
@@ -0,0 +1,26 @@
+package cat.itacademy.s04.t02.n01.fruit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Document(collection = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    private String id;
+
+    private String clientName;
+
+    private LocalDate deliveryDate;
+
+    private List<OrderItem> items;
+}
---
+++ fruit-api-h2/src/main/java/cat/itacademy/s04/t02/n01/fruit/model/OrderItem.java
@@ -0,0 +1,15 @@
+package cat.itacademy.s04.t02.n01.fruit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    private String fruitName;
+
+    private int quantityInKilos;
+}
